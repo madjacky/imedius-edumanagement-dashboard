12a80870b74ae0be801984dcb896940d4c1b76ee
madjacky <jacky.iakovenko@gmail.com>
2024-10-04 20:39:55 +0400

    add pagination

--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { role, teachersData } from '@/lib/data';
+import { role } from '@/lib/data';
 import TableSearch from '@/components/TableSearch'
 import { VscSettings } from "react-icons/vsc";
 import { FaSortAmountDown } from "react-icons/fa";
@@ -9,18 +9,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { BiShow } from "react-icons/bi";
 import FormModal from '@/components/FormModal';
+import { Class, Prisma, Subject, Teacher } from '@prisma/client';
+import prisma from '@/lib/prisma';
+import { ITEM_PER_PAGE } from '@/lib/settings';
 
-type Teacher = {
-  id: number;
-  teacherId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-}
+type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
 
 const columns = [
   {
@@ -58,37 +51,79 @@ const columns = [
   },
 ]
 
-export default function TeacherListPage() {
+const renderRow = (item: TeacherList) => {
+  return (
+    <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-imediusPurpleLight'>
+      <td className='flex items-center gap-4 p-4'>
+        <Image className='h-10 w-10 circle-avatar' src={item.img || "/images/noAvatar.png"} width={48} height={48} alt={item.name} />
+        <div className="flex flex-col gap-1">
+          <h3 className='font-semibold'>{item.name}</h3>
+          <span className='text-xs text-gray-500'>{item?.email}</span>
+        </div>
+      </td>
+      <td className='hidden md:table-cell'>{item.username}</td>
+      <td className='hidden md:table-cell'>{item.subjects.map(subject => subject.name).join(',')}</td>
+      <td className='hidden md:table-cell'>{item.classes.map(classItem => classItem.name).join(',')}</td>
+      <td className='hidden md:table-cell'>{item.phone}</td>
+      <td className='hidden md:table-cell'>{item.address}</td>
+      <td className=''>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`} className='flex items-center justify-center h-7 w-7 rounded-full bg-imediusSky'>
+            <BiShow className='text-white' size={16} />
+            <span className='sr-only'>Show Selected Teacher Single Page</span>
+          </Link>
+          {role === 'admin' && (
+            <FormModal table='teacher' type='delete' id={item.id} />
+          )}
+        </div>
+      </td>
+    </tr>
+  )
+}
 
-  const renderRow = (item: Teacher) => {
-    return (
-      <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-imediusPurpleLight'>
-        <td className='flex items-center gap-4 p-4'>
-          <Image className='h-10 w-10 circle-avatar' src={item.photo} width={48} height={48} alt={item.name} />
-          <div className="flex flex-col gap-1">
-            <h3 className='font-semibold'>{item.name}</h3>
-            <span className='text-xs text-gray-500'>{item?.email}</span>
-          </div>
-        </td>
-        <td className='hidden md:table-cell'>{item.teacherId}</td>
-        <td className='hidden md:table-cell'>{item.subjects.join(',')}</td>
-        <td className='hidden md:table-cell'>{item.classes.join(',')}</td>
-        <td className='hidden md:table-cell'>{item.phone}</td>
-        <td className='hidden md:table-cell'>{item.address}</td>
-        <td className=''>
-          <div className="flex items-center gap-2">
-            <Link href={`/list/teachers/${item.id}`} className='flex items-center justify-center h-7 w-7 rounded-full bg-imediusSky'>
-              <BiShow className='text-white' size={16} />
-              <span className='sr-only'>Show Selected Teacher Single Page</span>
-            </Link>
-            {role === 'admin' && (
-              <FormModal table='teacher' type='delete' id={item.id} />
-            )}
-          </div>
-        </td>
-      </tr>
-    )
+export default async function TeacherListPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+  const query: Prisma.TeacherWhereInput = {};
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+            break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
   }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.teacher.findMany({
+      where: query,
+      include: {
+        subjects: true,
+        classes: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.teacher.count({ where: query }),
+  ]);
+  console.log(data);
 
   return (
     <div className='flex flex-col gap-4 flex-1 m-4 mt-0 p-4 rounded-xl bg-white'>
@@ -111,8 +146,8 @@ export default function TeacherListPage() {
           </div>
         </div>
       </header>
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
-      <Pagination />
+      <Table columns={columns} renderRow={renderRow} data={data} />
+      <Pagination page={p} count={count} />
     </div>
   )
 }
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,18 +1,62 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation';
+import { ITEM_PER_PAGE } from "@/lib/settings";
 import Link from 'next/link'
 
-export default function Pagination() {
+export default function Pagination({ page, count }: { page: number; count: number }) {
+  const router = useRouter();
+  const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
+  const hasNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
+  
+  const changePage = (newPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
+
   return (
     <div className='flex items-center justify-between gap-4 text-gray-500'>
-      <button type='button' className='py-2 px-4 rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed' disabled>Prev</button>
+      <button 
+        onClick={() => {changePage(page - 1);}} 
+        type='button' 
+        className='py-2 px-4 rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed' 
+        disabled={!hasPrev}
+      >
+        Prev
+      </button>
       <ul className='flex items-center gap-2 text-sm' role='list'>
-        <li><Link href='/' className='px-2 rounded-sm bg-imediusSky'>1</Link></li>
-        <li><Link href='/' className='px-2 rounded-sm'>2</Link></li>
-        <li><Link href='/' className='px-2 rounded-sm'>3</Link></li>
-        <li><Link href='/' className='px-2 rounded-sm'>...</Link></li>
-        <li><Link href='/' className='px-2 rounded-sm'>10</Link></li>
+        {Array.from(
+          { length: Math.ceil(count / ITEM_PER_PAGE) },
+          (_, index) => {
+            const pageIndex = index + 1;
+            return (
+              <li key={pageIndex}>
+                <Link
+                  href={`${window.location.pathname}?page=${pageIndex}`}
+                  className={`px-2 rounded-sm ${
+                    page === pageIndex ? "bg-imediusSky" : ""
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    changePage(pageIndex);
+                  }}
+                >
+                  {pageIndex}
+                </Link>
+              </li>
+            );
+          }
+        )}
       </ul>
-      <button type='button'  className='py-2 px-4 rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed'>Next</button>
+      <button 
+        onClick={() => {changePage(page + 1);}} 
+        type='button'  
+        className='py-2 px-4 rounded-md bg-slate-200 disabled:opacity-50 disabled:cursor-not-allowed'
+        disabled={!hasNext}
+      >
+        Next
+      </button>
     </div>
   )
 }
